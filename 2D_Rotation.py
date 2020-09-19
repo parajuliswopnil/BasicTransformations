3446566e89a1bd507e4b10f2945d84fbaa8b310a
parajuliswopnil <38814682+parajuliswopnil@users.noreply.github.com>
2020-09-19 20:13:14 +0545

    2D rotation completed

---
+++ 2D_Rotation.py
@@ -0,0 +1,76 @@
+import pygame
+import sys
+from pygame.locals import QUIT
+import pygame.gfxdraw
+import math
+
+pygame.init()
+
+colorBlack = (0, 0, 0)
+colorRed = (255, 0, 0)
+colorGreen = (0, 255, 0)
+colorWhite = (255, 255, 255)
+
+coordinatesOfFigure = []
+transformationVector = []
+originalFigure = []
+
+figureSides = int(input("Enter the total number of sides of the figure: "))
+for i in range(figureSides):
+    x1 = int(input("Enter the x coordinate of the side " + str(i) + ": "))
+    y1 = int(input("Enter the y coordinate of the side " + str(i) + ": "))
+    originalFigure.append((x1, y1))
+    coordinatesOfFigure.append([x1, y1, 1])
+
+rotationAngleDegrees = int(input("Enter the rotation angle anticlockwise about the origin: "))
+rotationAngleRadian = (math.pi / 180) * rotationAngleDegrees
+transformationVector.append((math.cos(rotationAngleRadian), -math.sin(rotationAngleRadian), 0))
+transformationVector.append((math.sin(rotationAngleRadian), math.cos(rotationAngleRadian), 0))
+transformationVector.append((0, 0, 1))
+
+transposedCoordinate = [[0 for i in range(len(coordinatesOfFigure))] for j in range(len(coordinatesOfFigure[0]))]
+for i in range(len(coordinatesOfFigure[0])):
+    for j in range(len(coordinatesOfFigure)):
+        transposedCoordinate[i][j] = coordinatesOfFigure[j][i]
+
+finalTransformedCoordinate = [[0 for i in range(len(transposedCoordinate[0]))] for j in
+                              range(len(transformationVector))]
+
+for i in range(len(transformationVector)):
+    for j in range(len(transposedCoordinate[0])):
+        for k in range(len(transposedCoordinate)):
+            finalTransformedCoordinate[i][j] += transformationVector[i][k] * transposedCoordinate[k][j]
+
+transposedFinalCoordinate = [[0 for i in range(len(finalTransformedCoordinate))] for j in
+                             range(len(finalTransformedCoordinate[0]))]
+for i in range(len(finalTransformedCoordinate[0])):
+    for j in range(len(finalTransformedCoordinate)):
+        transposedFinalCoordinate[i][j] = finalTransformedCoordinate[j][i]
+
+# print(finalTransformedCoordinate)
+
+
+print(coordinatesOfFigure)
+print(transposedCoordinate)
+print(finalTransformedCoordinate)
+print(transposedFinalCoordinate)
+
+for i in range(len(transposedFinalCoordinate)):
+    transposedFinalCoordinate[i].remove(1)
+
+print(transposedFinalCoordinate)
+
+drawingSurface = pygame.display.set_mode((900, 900))
+drawingSurface.fill(colorWhite)
+pygame.display.set_caption("2D Translation")
+pygame.draw.polygon(drawingSurface, colorGreen, transposedFinalCoordinate)
+pygame.draw.polygon(drawingSurface, colorRed, originalFigure)
+
+pygame.display.update()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+# print(transformationVector)
---
+++ test.py
@@ -0,0 +1,5 @@
+import math
+x = math.pi / 2
+y = math.sin(x)
+print(y)
+print(x)
